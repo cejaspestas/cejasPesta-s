fbba88eeeccd2fa0b26314e0b0964bb36a7eb07e
jhoan <johanmonsalve125@gmail.com>
2024-12-16 10:49:16 -0500

    15.0

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,11 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client";
 
-export const db = new PrismaClient()
+// Evitar múltiples instancias de PrismaClient
+const prismaGlobal = global as typeof global & { prisma?: PrismaClient };
+
+// Si no existe una instancia, creamos una
+export const db = prismaGlobal.prisma ?? new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+    prismaGlobal.prisma = db;
+}
