957b52ebb144f0a97fb6464bce809cc5a61fff0f
jhoan <johanmonsalve125@gmail.com>
2024-12-18 13:17:06 -0500

    510.0

--- context/fetchdatos.tsx
+++ context/fetchdatos.tsx
@@ -3,70 +3,109 @@ import { Cliente, Contacto, Equipo, Fecha, Imagen, Precio, Product, Promocion, S
 import { createContext, useState, useEffect, ReactNode, useContext } from "react";
 
 type Props = {
-  contactos: Contacto[];
-  imagenes: Imagen[];
-  servicios: Servicio[];
-  videos: Video[];
-  clientes: Cliente[];
-  precios: Precio[];
-  promociones: Promocion[];
-  equipo: Equipo[];
-  productos: Product[];
-  dataUser: UserInfo[];
-  fecha: Fecha[];
+    contactos: Contacto[];
+    imagenes: Imagen[];
+    servicios: Servicio[];
+    videos: Video[];
+    clientes: Cliente[];
+    precios: Precio[];
+    promociones: Promocion[];
+    equipo: Equipo[];
+    productos: Product[];
+    dataUser: UserInfo[];
+    fecha: Fecha[];
 } | null;
 
 const DataContext = createContext<Props>(null);
 
 export const DataProvider = ({ children }: { children: ReactNode }) => {
-    const [contactos , setContactos] = useState<Contacto[]>([]);
-    const [imagenes , setImagenes] = useState<Imagen[]>([]);
-    const [servicios , setServicios] = useState<Servicio[]>([]);
-    const [videos , setVideos] = useState<Video[]>([]);
-    const [clientes , setClientes] = useState<Cliente[]>([]);
-    const [precios , setPrecios] = useState<Precio[]>([]);
-    const [promociones , setPromociones] = useState<Promocion[]>([]);
-    const [equipo , setEquipo] = useState<Equipo[]>([]);
-    const [productos , setProductos] = useState<Product[]>([]);
-    const [dataUser , setDataUser] = useState<UserInfo[]>([]);
-    const [fecha , setFecha] = useState<Fecha[]>([]);
+    const [contactos, setContactos] = useState<Contacto[]>([]);
+    const [imagenes, setImagenes] = useState<Imagen[]>([]);
+    const [servicios, setServicios] = useState<Servicio[]>([]);
+    const [videos, setVideos] = useState<Video[]>([]);
+    const [clientes, setClientes] = useState<Cliente[]>([]);
+    const [precios, setPrecios] = useState<Precio[]>([]);
+    const [promociones, setPromociones] = useState<Promocion[]>([]);
+    const [equipo, setEquipo] = useState<Equipo[]>([]);
+    const [productos, setProductos] = useState<Product[]>([]);
+    const [dataUser, setDataUser] = useState<UserInfo[]>([]);
+    const [fecha, setFecha] = useState<Fecha[]>([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('/api/datosdatos', {
-                    method : 'GET',
+                const storedData = sessionStorage.getItem("dataContext");
+                const parsedData = JSON.parse(storedData || "{}");
+
+                if (storedData) {
+    
+                    // Establece los datos del almacenamiento en el estado
+                    setContactos(parsedData.contactos);
+                    setImagenes(parsedData.imagenes);
+                    setServicios(parsedData.servicios);
+                    setVideos(parsedData.videos);
+                    setClientes(parsedData.clientes);
+                    setPrecios(parsedData.precios);
+                    setPromociones(parsedData.promociones);
+                    setEquipo(parsedData.equipo);
+                    setProductos(parsedData.productos);
+                    setFecha(parsedData.fecha);
+                }
+    
+                // Haz la petición a la API
+                const response = await fetch("/api/datosdatos", {
+                    method: "GET",
                     headers: {
-                        'Content-Type': 'application/json',
+                        "Content-Type": "application/json",
                     },
                 });
-
+    
                 if (response.ok) {
                     const data = await response.json();
                     const { contactos, imagenes, servicios, videos, clientes, precios, promociones, equipo, productos, dataUser, fecha } = data.info;
-
-                    // Update state with the fetched data
-                    setContactos(contactos);
-                    setImagenes(imagenes);
-                    setServicios(servicios);
-                    setVideos(videos);
-                    setClientes(clientes);
-                    setPrecios(precios);
-                    setPromociones(promociones);
-                    setEquipo(equipo);
-                    setProductos(productos);
-                    setDataUser(dataUser);
-                    setFecha(fecha);
+    
+                    // Verifica si los datos obtenidos son diferentes de los almacenados
+                    const newData = {
+                        contactos,
+                        imagenes,
+                        servicios,
+                        videos,
+                        clientes,
+                        precios,
+                        promociones,
+                        equipo,
+                        productos,
+                        fecha,
+                    };
+    
+                    if (!storedData || JSON.stringify(parsedData) !== JSON.stringify(newData)) {
+                        // Actualiza el estado con los nuevos datos
+                        setContactos(contactos);
+                        setImagenes(imagenes);
+                        setServicios(servicios);
+                        setVideos(videos);
+                        setClientes(clientes);
+                        setPrecios(precios);
+                        setPromociones(promociones);
+                        setEquipo(equipo);
+                        setProductos(productos);
+                        setDataUser(dataUser);
+                        setFecha(fecha);
+    
+                        // Guarda los nuevos datos en sessionStorage
+                        sessionStorage.setItem("dataContext", JSON.stringify(newData));
+                    }
                 } else {
                     console.error("Failed to fetch data");
                 }
             } catch (error) {
-                console.error(error instanceof Error ? "Error fetching data:": "");
+                console.error(error instanceof Error ? "Error fetching data:" : error);
             }
         };
-
+    
         fetchData();
-    }, []); // Empty dependency array to run only once on component mount
+    }, []); // Empty dependency array para ejecutar solo al montar el componente
+    
 
     const value = {
         contactos,
@@ -79,14 +118,10 @@ export const DataProvider = ({ children }: { children: ReactNode }) => {
         equipo,
         productos,
         dataUser,
-        fecha
+        fecha,
     };
 
-    return (
-        <DataContext.Provider value={value}>
-            {children}
-        </DataContext.Provider>
-    );
+    return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
 };
 
-export const useData = () => useContext(DataContext); // Hook to use the context in child components
+export const useData = () => useContext(DataContext);
